2b83b1a3dc21cd47e3fb15c4da9addfaa80ba6d8
rickeymaharjan <rickeymhrjn23@gmail.com>
2023-10-08 07:29:47 +0545

    Phonebook backend ex3.4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -53,6 +53,13 @@ app.get("/api/persons/:id", (request, response) => {
   }
 
   response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((p) => p.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3000;
