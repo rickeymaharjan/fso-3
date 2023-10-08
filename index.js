5cb5df1317cfad8cdc3910ac1be58281938fc57d
rickeymaharjan <rickeymhrjn23@gmail.com>
2023-10-08 10:36:04 +0545

    Phonebook backend ex3.8

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const { response } = require("express");
+const morgan = require("morgan");
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(req.body),
+    ].join(" ");
+  })
+);
 
 let persons = [
   {
